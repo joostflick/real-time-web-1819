7fec94a7114134bfb44923c12ad071336aa200b1
Joost Flick <joostflick@gmail.com>
2019-04-09 14:16:08 +0200

    simplified code structure

--- app.js
+++ app.js
@@ -1,44 +1,33 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
 
-var app = express()
+const path = require('path')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+const io = require('socket.io')(http)
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+const port = 7070
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(
-  '/scripts',
-  express.static(path.join(__dirname, '/node_modules/socket.io-client/dist'))
-)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'public/views'))
 
-app.get('/', function(req, res) {
-  res.render('index.ejs')
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    console.log('Message: ' + msg)
+    io.emit('chat message', msg)
+  })
+  console.log('A user connected')
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
 })
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+http.listen(port, () => {
+  console.log(`App running on port ${port}!`)
 })
-
-module.exports = app
