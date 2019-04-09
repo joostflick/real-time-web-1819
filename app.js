b8ee444ed9f53ed8b2e8460740c0fd431c56f267
Joost Flick <joostflick@gmail.com>
2019-04-09 14:52:16 +0200

    added db storage

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
+var mongoose = require('mongoose')
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var dbUrl = 'mongodb://messages:messages1@ds225492.mlab.com:25492/messages'
 
 const path = require('path')
 
@@ -9,6 +16,12 @@ const io = require('socket.io')(http)
 const port = 7070
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+mongoose.connect(dbUrl, err => {
+  console.log(err || 'mongodb connected')
+})
+
+var Message = mongoose.model('Message', { name: String, message: String })
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'public/views'))
@@ -19,8 +32,12 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   socket.on('chat message', msg => {
-    console.log('Message: ' + msg)
-    io.emit('chat message', msg)
+    if (msg) {
+      io.emit('chat message', msg)
+      Message.create({ name: 'onbekend', message: msg }).then(
+        console.log(msg + ' stored in db')
+      )
+    }
   })
   console.log('A user connected')
   socket.on('disconnect', () => {
