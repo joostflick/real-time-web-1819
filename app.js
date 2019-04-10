2b4a5430c5d0b8cdeb81093536cafc33ae917c31
Joost Flick <joostflick@gmail.com>
2019-04-10 11:27:02 +0200

    translate functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 var mongoose = require('mongoose')
+require('isomorphic-fetch')
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -30,13 +31,62 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
+var sourceLang = 'auto'
+var targetLang = 'en'
+
 io.on('connection', socket => {
   socket.on('chat message', msg => {
     if (msg) {
-      io.emit('chat message', msg)
-      Message.create({ name: 'onbekend', message: msg }).then(
-        console.log(msg + ' stored in db')
-      )
+      var sourceText = msg
+      var url =
+        'https://translate.googleapis.com/translate_a/single?client=gtx&sl=' +
+        sourceLang +
+        '&tl=' +
+        targetLang +
+        '&dt=t&q=' +
+        encodeURI(sourceText)
+      fetch(url)
+        .then(function(response) {
+          return response.json()
+        })
+        .then(function(myJson) {
+          var newMsg = JSON.stringify(myJson[0][0][0])
+          var oldMsg = JSON.stringify(myJson[0][0][1])
+          var lang = JSON.stringify(myJson[2])
+          console.log(newMsg)
+          io.emit(
+            'chat message',
+            // 'Recognized language: ' +
+            //   lang +
+            //   ' Message: ' +
+            //   oldMsg +
+            //   ' Translated to English: ' +
+            newMsg
+          )
+        })
+      // Message.create({ name: 'onbekend', message: newMsg }).then(
+      //   console.log(newMsg + ' stored in db')
+      // )
     }
   })
   console.log('A user connected')
