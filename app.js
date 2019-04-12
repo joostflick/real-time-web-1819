d7b1e5922c1c36ba1ef1b2375bc36fe4750506a9
Joost Flick <joostflick@gmail.com>
2019-04-12 11:16:02 +0200

    disconnect message

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ io.on('connection', socket => {
   })
   console.log('A user connected')
   socket.on('disconnect', () => {
-    console.log('A user disconnected')
+    io.emit('bot message', username + ' disconnected.')
   })
 })
 
