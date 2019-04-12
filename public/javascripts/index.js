b2af9ae43e32e99e9e89b3bc91f289bf355530d5
Joost Flick <joostflick@gmail.com>
2019-04-12 11:56:44 +0200

    added scroll

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,16 +8,20 @@ $(function() {
   })
   socket.on('chat message', function(msg) {
     $('#messages').append($('<li>').text(msg))
+    $('#chat-end')[0].scrollIntoView()
   })
   socket.on('username', function(username) {
     $('#messages').append($('<li class="username">').text(username))
+    $('#chat-end')[0].scrollIntoView()
   })
   socket.on('bot message', function(msg) {
     $('#messages').append($('<li class="botmessage">').text(msg))
+    $('#chat-end')[0].scrollIntoView()
   })
   socket.on('link', function(msg) {
     $('#messages').append(
       $('<a href=' + msg + ' target="_blank"><li>').text(msg)
     )
+    $('#chat-end')[0].scrollIntoView()
   })
 })
